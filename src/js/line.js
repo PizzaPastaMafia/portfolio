a61334e2ed6292c03639455edbe3213ba48b7c54
Lorenzo Del Forno <lorendelfo@protonmail.com>
2022-11-16 18:14:30 +0100

    working on image drawing, added labels, removed threejs

--- src/js/line.js
+++ src/js/line.js
@@ -1,14 +1,36 @@
 var prevX = mouseX;
 var prevY = mouseY;
+var img;
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
     fill(126);
-    background(102);
+    background(255);
     stroke(Math.random()*200);
+    img = loadImage('../assets/moonwalk.png');
+    //img[1] = loadImage('assets/img1.jpg'); // Load the image
+    //img[2] = loadImage('assets/img2.jpg'); // Load the image
+    //for (let i=0; i<3; i++) {
+        image(img, 0,0);
+    //}
+    
 }
 function draw() {
     line(mouseX, mouseY, prevX, prevY); 
     prevX = mouseX;
     prevY = mouseY;
+
 }
+
+gsap.fromTo(".class", {
+    x: 100, //normal value
+    y: function(index, target, targets) { //function-based value
+        return index * 50; 
+    }
+},
+{
+    x: 50,
+    y: 0,
+    duration: 1
+});
 
